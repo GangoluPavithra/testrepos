ca3f0a5927c99d66a667527830a0dbdd45b076d7
Pavithra Gangolu <PavithraG@fixitytech.com>
2022-12-13 15:22:16 +0530

    firstcommit

---
+++ POPUP/Data Context/TransactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace POPUP.Models
+{
+
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<TransactionModel> Transaction { get; set; }
+    }
+
+
+}
